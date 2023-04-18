fdcda7f762261ddacf0a78dcf1130e039e38841f
Fumo <herrscher.of.sleeping@protonmail.ch>
2023-04-18 13:19:11 +0200

    Fix indentation (tabs -> spaces)

--- commands/checkpoint.lua
+++ commands/checkpoint.lua
@@ -9,10 +9,10 @@ local function run(model, args)
   end
   local last_line = model:get_tracking_info_at_pos(1)
   if not last_line or last_line.action ~= "start" then
-		return false, "Task hasn't been started"
-	end
+    return false, "Task hasn't been started"
+  end
   local time = util.time.current_time()
-	local ok, err = model.commands.stop(time, description)
+  local ok, err = model.commands.stop(time, description)
   if ok then
     ok, err = model.commands.start(time, last_line.task)
   end
